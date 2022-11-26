9bcb8e71a1f122231c49fc3b17888b1bdcbf4815
Dmitriy Ivanov <ivanov.dmitriy.01031990@gmail.com>
2022-11-26 10:27:11 +0500

    002 Стратегия Итерация

--- computer-science/002-strategy-iteration/GITHUB_crash10390_Dmitrii.py
+++ computer-science/002-strategy-iteration/GITHUB_crash10390_Dmitrii.py
@@ -14,32 +14,3 @@ while i<n:
         list3.append(list[i])
         i+=1
 print(*list3, sep="\n", end="\n\n") 
-#print(*list3)
-
-
-
-
-
-#def merge (list, list2):
-#    result = []
-#list3=[list + list2]
-##print (list3)
-#while not [list and list2]:
-#    if list[0] > list2[0]:
-#        list3 == list.remove[0]
-##        print (list3)
-#    else:
-#        list3 == list2.remove[0]
-#        result.append (list3)
-##    else:
-##        list3 == list2.pop(0)
-#print (*list3)
-
-
-#while not (list.empty and list2.empty)
-# if list.top_item>list2.top_item
-# else
-#    list = list.remove_top_item
-#result.append(fish)
-#return result
-#print (list3)
