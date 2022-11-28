44e12f067fb1b8c87678975965d5768bedaf22c5
Dmitriy Ivanov <ivanov.dmitriy.01031990@gmail.com>
2022-11-28 08:58:26 +0500

    002 Стратегия Итерация(2)

--- computer-science/002-strategy-iteration/GITHUB_crash10390_Dmitrii.py
+++ computer-science/002-strategy-iteration/GITHUB_crash10390_Dmitrii.py
@@ -1,4 +1,3 @@
-n, m = 4,3
 list = ['Asp','Carp','Ide','Trout']
 list2 = ['Cod','Herring','Marlin']
 i = j = 0
@@ -10,7 +9,4 @@ while i<n and j<m:
     else:
         list3.append(list2[j])
         j+=1
-while i<n:
-        list3.append(list[i])
-        i+=1
-print(*list3, sep="\n", end="\n\n") 
+print(list3, sep="\n", end="\n\n") 
